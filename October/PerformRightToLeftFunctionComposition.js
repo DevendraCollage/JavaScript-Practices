3ecb9030f4623d78d521c1524c0bb1f5b933ec2a
Devendra Parmar <23030501043@darshan.ac.in>
2024-10-10 12:56:29 +0530

    Solved the two questions of the JavaScript 1. Add special character ot print color to the console 2. Perform right to left function composition

---
+++ October/PerformRightToLeftFunctionComposition.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to perform right-to-left function composition.
+
+const rmultiplyAndAdd5 = (a, b) => {
+  return a * b + 5;
+};
+
+console.log(rmultiplyAndAdd5(5, 2));
+console.log(rmultiplyAndAdd5(9, 5));
